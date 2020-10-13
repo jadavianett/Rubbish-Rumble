36191fc96340d8df42c34bf0b6803a6f9a7ce332
samis <samilsully@gmail.com>
2020-10-13 11:10:28 -0700

    Added in the base for handlebars

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const exphbs = require("express-handlebars");
+const handlebars = require("handlebars");
 
 const PORT = process.env.PORT || 8080;
 
@@ -7,11 +9,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 app.get("/api/config", (req, res) => {
-    res.json({
-      success: true,
-    });
+  res.json({
+    success: true,
   });
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
