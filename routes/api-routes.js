7baf3de97a2370e44c9e8698f6c4bddeb233486e
Nemancer <cddeal12@gmail.com>
2020-10-14 08:07:08 -0700

    Added new character post route

---
+++ routes/api-routes.js
@@ -0,0 +1,21 @@
+// Requires models to interact with sequelize
+const db = require("../models");
+
+// Exports all api routes
+module.exports = function(app) {
+
+    app.post("/api/addCharacter", function(req,res) {
+        // Creates a new character based on the incoming request
+        db.Character.create({
+            character_name: req.body.character_name,
+            advantage: req.body.advantage,
+            avatar_image: req.body.avatar_image,
+            wins: req.body.wins,
+            losses: req.body.losses,
+            user_id: req.body.user_id
+        }).then(function(newCharacter) {
+            res.json(newCharacter);
+        });
+    });
+
+};
