c2daa4dbbd8a17a0c55040d708e6c1ed443eb88b
Jada Arnett <jada.arnett@gmail.com>
2020-10-15 11:03:23 -0400

    adding functionality to log user input for the character created

--- public/assets/js/createCharacter.js
+++ public/assets/js/createCharacter.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
     $('.carousel').carousel();
+
+    $("#view-all-characters").click(function (event) {
+      console.log("The view all characters button was clicked");
+      let newCharacter = {
+        character_name: $("#character-name-form [name=name]").val().trim(),
+        advantage: $("#advantage-chosen [name=advantage]:checked").val(),
+      };
+      console.log(newCharacter);
+      //save character information 
+      // re route to the view all characters page
+    
+        })
+    
+    $("#add-new-character").click(function (event) {
+      console.log("The add new character button was clicked");
+      // save character information 
+      // let newCharacter = {
+      //   name: $("#character-name [name=character-name-form]").val().trim(),
+      // };
+      // console.log(newCharacter);
+      //refresh the page 
+      location.reload();
+    })
   });
