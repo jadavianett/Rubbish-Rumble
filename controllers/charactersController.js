a04c447d1e16e55eed96355778d01b0fae56e735
Nemancer <cddeal12@gmail.com>
2020-10-14 18:15:16 -0700

    Added route to update character object and save it to database

--- controllers/charactersController.js
+++ controllers/charactersController.js
@@ -10,7 +10,7 @@ const db = require("../models");
 // ======================================================
 
 // adds a new character based on inputted req.body
-router.post("/api/addCharacter", function(req,res) {
+router.post("/api/character", function(req,res) {
     // Creates a new character based on the incoming request
     db.Character.create({
         character_name: req.body.character_name,
@@ -35,7 +35,7 @@ router.post("/api/addCharacter", function(req,res) {
 });
 
 // gets all characters belonging to a certain user id for viewing
-router.get("/api/viewCharacters/:id", function(req,res) {
+router.get("/api/character/:id", function(req,res) {
     db.Character.findAll({
         where: {
             user_id: req.params.id
@@ -43,6 +43,52 @@ router.get("/api/viewCharacters/:id", function(req,res) {
     }).then(matchedCharacters => {
         console.log("Found your characters")
         res.json(matchedCharacters);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json({
+            error: true,
+            data: null,
+            message: "Unable to find characters",
+        });
+    });
+});
+
+// deletes a chracter that has a certain id
+router.delete("/api/character/:id", function(req,res) {
+    db.Character.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(deletedCharacter => {
+        console.log("deleted character successfully"); 
+        res.end();
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json({
+            error: true,
+            data: null,
+            message: "Unable to delete",
+        });
+    });
+});
+
+
+// updates a character with a certain id
+router.put("/api/character/:id", function(req,res) {
+    db.Character.findOner({
+        where: {
+            id: req.params.id
+        }
+    }).then((characterToUpdate) => {
+        console.log("Updating character")
+        // Updates the character's name, stats, and win/loss record, then saves it
+        characterToUpdate.character_name = req.body.character_name;
+        characterToUpdate.hp = req.body.hp;
+        characterToUpdate.atk = req.body.atk;
+        characterToUpdate.def = req.body.def;
+        characterToUpdate.wins = req.body.wins;
+        characterToUpdate.losses = req.body.losses;
+        characterToUpdate.save();
     });
 });
 
