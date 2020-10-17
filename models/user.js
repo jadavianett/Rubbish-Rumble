19626b9ae9b08bd0cd7df3e4bb46de0fc99aae58
samis <samilsully@gmail.com>
2020-10-17 10:32:20 -0700

    Edited user model to fixed foreign key issues

--- models/user.js
+++ models/user.js
@@ -1,22 +1,29 @@
 // Require models for linking with other models
 module.exports = function (sequelize, DataTypes) {
-    const User = sequelize.define("User", {
-        // id: {
-        //     type: DataTypes.UUID,
-        //     primaryKey: true,
-        //     defaultValue: DataTypes.UUIDV4
-        // },
-        user_name: {
-            type: DataTypes.STRING,
-        },
-
+  const User = sequelize.define(
+    "User",
+    {
+      // id: {
+      //     type: DataTypes.UUID,
+      //     primaryKey: true,
+      //     defaultValue: DataTypes.UUIDV4
+      // },
+      user_name: {
+        type: DataTypes.STRING,
+      },
     },
     {
-        timestamps: false,
-        underscored: true,
+      timestamps: false,
+      underscored: true,
+    }
+  );
+
+  User.associate = function (models) {
+    User.belongsToMany(models.Character, {
+      through: "UserCharacters",
+      foreignKey: "character_id",
     });
+  };
 
-    
-
-    return User;
+  return User;
 };
